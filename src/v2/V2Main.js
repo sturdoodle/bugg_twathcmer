aa3daa22badecdcdc388900fa10b54abc842bc22
sturdoodle <113435474+sturdoodle@users.noreply.github.com>
2026-04-25 19:55:42 +0530

    fxxxx

--- src/v2/V2Main.js
+++ src/v2/V2Main.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './v2.css';
 import { useFocus } from './useFocusStore';
